db381838c459fc8f34d0cfdc8a949a4f23d529ee
Tajinder Singh <singhtajinder9888@gmail.com>
2025-12-31 23:42:33 +0530

    Initiated the neo-code project without dist.

--- backend/dist/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export type FileTree = {
-    name: string;
-    isDir: boolean;
-    content: string | undefined;
-    children?: FileTree[] | undefined;
-    path?: string | undefined;
-};
-//# sourceMappingURL=index.d.ts.map
--- backend/dist/utils/treeGenerator.d.ts
+++
@@ -1,4 +0,0 @@
-import type { FileTree } from "../types/index.js";
-declare const generateFileTree: (dir: string) => Promise<FileTree>;
-export default generateFileTree;
-//# sourceMappingURL=treeGenerator.d.ts.map
